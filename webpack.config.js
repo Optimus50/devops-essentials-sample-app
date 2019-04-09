166f6106b2a2db6677473dee48482eb52f9aa6b9
ben <benyforu2020@gmail.com>
2019-04-09 17:44:49 +0200

    Added default state

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+
+   mode: "development",
+   entry: path.resolve(__dirname, `src`, `app`),
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/'
+   },
+   resolve: {
+      extensions: ['.js', '.jsx']
+   },
+   devServer: {
+      historyApiFallback: true
+   },
+   module: {
+      rules: [{
+         test: /\.jsx?/,
+         exclude: /(node_modules|bower_components)/,
+         use: {
+            loader: "babel-loader",
+            options: {
+               presets: ['@babel/preset-env']
+            }
+         }
+      }]
+   }
+}
